59d605671e22c860d40cf881748571e801568adb
Dongyoung kim <kdy0415@dodoom.co.kr>
2023-02-03 18:33:42 +0900

    dd

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,14 @@
 import Layout from "../components/layout";
+import Head from "next/head";
 
 export default function About(){
     return (
         <Layout>
+            <Head>
+                <title>리액트</title>
+                <meta name="description" content="리액트 공부" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <h1>소개페이지</h1>
         </Layout>
     )
